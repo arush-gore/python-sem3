d01c756262144deeda0154faa0739f21a5419190
arush-gore <arushgore.rawfiles@gmail.com>
2026-08-14 13:57:27 +0530

    Add files via upload

---
+++ Lecture 7 (1).py
@@ -0,0 +1,37 @@
+"""#Driving license eligible or not 
+age = int(input("Enter your age:"))
+if(age>=18):
+   print("Congratulations , you are e;ligible foe driving license") 
+else:
+    print ("Sorry, you are not eligible for license")"""
+
+"""   #wether u need to buy a ticket or not 
+height = int(input("Enter your height in feet"))
+if(height>=3): print("You need to buy a ticket")
+else: print("You do not need a ticket")"""
+
+"""#
+height=int(input("Enter you height in feet"))
+if(height>=3):print("Buy a ticket"), print("You are on board")"""
+
+"""#
+height=float(input("Enter you height in feet"))
+if(height>=3):print("Buy a ticket"), print("You are on board")"""
+
+"""#
+a=55
+if a%2==0:
+    print("even")
+if(a>30):
+    print("number is greater than 30")
+    print("bye")"""
+
+   """ #
+a = int(input("enter the value:"))
+if a%2==0:
+    print("even")
+else: print("odd")
+if(a>30):
+    print("number is greater than 30")
+else:
+    print("Bye")"""
